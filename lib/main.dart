fc3207083e7db03a00c19abc8629379e35674f4a
Esther <chisneya96@gmail.com>
2024-03-12 16:52:40 +0100

    test app design for flutter

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset(
+                'assets/images/image.jpg',
+                width: 500,
+                height: 500,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "Organize your life with Habbio",
+                style: TextStyle(
+                  fontSize: 29,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              alignment: Alignment.center,
+              child: const Text(
+                "Habbio is a free and simple daily\n habit tracker",
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  // padding:const EdgeInsets.all(18.0),
+                  backgroundColor:const Color.fromARGB(255, 123, 36, 30),
+                foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                child: const Text(
+                  "Got it!",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
